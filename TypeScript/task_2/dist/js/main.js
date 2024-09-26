613a16ce64a9d32635c2cb4b93f12551d3b7a56c
Alex Guadalupe <9035@holbertonstudents.com>
2024-09-26 12:18:25 -0400

    task2

---
+++ TypeScript/task_2/dist/js/main.js
@@ -0,0 +1,10 @@
+// Example
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
+//# sourceMappingURL=main.js.map
