e948af0920a28f419b4843e45e2b379fb07d1c8b
Alex Guadalupe <9035@holbertonstudents.com>
2024-09-26 12:26:38 -0400

    task2

--- TypeScript/task_1/dist/js/main.js
+++ TypeScript/task_1/dist/js/main.js
@@ -17,4 +17,13 @@ var teacher2 = {
 };
 console.log(teacher1);
 console.log(teacher2);
+// Example
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
 //# sourceMappingURL=main.js.map
